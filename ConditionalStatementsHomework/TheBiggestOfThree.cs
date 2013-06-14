d3bffd4bb0f3213601871e68993b5714114043fa
Kiril Asenov <kicata_bg@yahoo.com>
2013-06-14 22:14:08 +0300

    ConditionalStatementsHomework

---
+++ ConditionalStatementsHomework/TheBiggestOfThree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+//Write a script that finds the biggest of three integers using nested if statements.
+
+class TheBiggestOfThree
+    {
+        static void Main()
+        {
+           Console.Write("Enter integer number a = ");
+           int firstValue =int.Parse(Console.ReadLine());
+
+           Console.Write("Enter integer number b = ");
+           int secondValue =int.Parse(Console.ReadLine());
+
+           Console.Write("Enter integer number c = ");
+           int thirdValue =int.Parse(Console.ReadLine());
+
+           int greater = thirdValue;
+
+           if ((firstValue > thirdValue)||(secondValue > thirdValue))
+           {
+               if (firstValue > secondValue)
+               {
+                   greater = firstValue;
+                   Console.WriteLine("The greater number is {0}",greater);
+               }
+               else
+               {
+                   greater = secondValue;
+                   Console.WriteLine("The greater number is {0}", greater);
+               }
+           }
+           else
+           {
+               greater = thirdValue;
+               Console.WriteLine("The greater number is {0}", greater);
+           }
+        }
+    }
+
---
+++ ConditionalStatementsHomework/UserInputTypes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+/* Write a program that, depending on the user's choice inputs int,
+ double or string variable. If the variable is integer or double, increases it with 1.
+ If the variable is string, appends "*" at its end. The program must show the value of that
+ variable as a console output. Use switch statement.
+ * */
+    class UserInputTypes
+    {
+        static void Main()
+        {
+            //reseting culture
+            Thread.CurrentThread.CurrentCulture =
+            CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter    1 for int\n \t 2 for double\n\t 3 for string");
+            byte userChoice = byte.Parse(Console.ReadLine());
+            
+            switch (userChoice)
+            {
+                case 1:
+                    Console.Write("Now type your variable = ");
+
+                    int intUserChoice = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Result is {0}",intUserChoice + 1);
+                    break;
+                case 2:
+                    Console.Write("Now type your variable = ");
+
+                    double doubleUserChoice = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Result is {0:0.000}",doubleUserChoice + 1);
+                    break;
+                case 3:
+                    Console.Write("Now type your variable = ");
+
+                    string stringUserChoice = Console.ReadLine();
+                    Console.WriteLine("Result is {0}", stringUserChoice + "*");
+                    break;
+                default: Console.WriteLine("Invalid input! Start again.....");
+                    break;
+            }
+        }
+    }
+
